9ef0dafd6651fe135114b2fdb28514ea4e2c693d
jaynson <lyghtshades@outlook.com>
2022-04-26 18:16:32 -0800

    Added nav nar and some dashboard elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BalanceComponent } from './components/balance/balance.component';
+import { LedgerComponent } from './components/ledger/ledger.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { UserAuthModule } from './user-auth/user-auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    BalanceComponent,
+    LedgerComponent,
+    TransactionComponent,
+    SummaryComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, UserAuthModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
